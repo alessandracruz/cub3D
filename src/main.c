d16044eb734db067d514c4bba231cd3f14d36b87
Vanessa <66659404+vanessachabs@users.noreply.github.com>
2024-04-06 19:02:13 +0000

    alteracoes-vanessa

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "cub3d.h"
+#include "cub3d.h"
 
 /*
 int	main(int argc, char **argv)
@@ -32,17 +32,39 @@ int	main(int argc, char **argv)
 }
 */
 
-#include "cub3d.h"
+int	is_cub(char *str)
+{
+	size_t	len;
+
+	len = ft_strlen(str);
+	if (len < 4)
+		return (0);
+	if (ft_strcmp((str + len - 4), ".cub"))
+		return (1);
+	return (0);
+}
+
+int	check_args(char *s[], int argc)
+{
+	if (argc != 2)
+	{
+		ft_printf("Usage: %s <map_file.cub>\n", s[0]);
+		return (0);
+	}
+	if (!is_cub(s[1]))
+	{
+		ft_printf("Usage: %s <map_file.cub>\n", s[0]);
+		return (0);
+	}
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
 	t_config	config;
 
-	if (argc != 2)
-	{
-		ft_printf("Usage: %s <map_file.cub>\n", argv[0]);
-		return (1);
-	}
+	if (!check_args(argv, argc))
+		exit (1);
 	ft_bzero(&config, sizeof(t_config));
 	if (read_map_config(argv[1], &config) == -1)
 	{
