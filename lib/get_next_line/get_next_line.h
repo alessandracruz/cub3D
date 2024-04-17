17e23b8c9016a19f9437a27d1ce9825e95ac783a
acastilh <acastilh@student.42.rio>
2024-04-17 18:57:49 -0300

    Refatoração da minha get_next_line que estava dando problema

--- lib/get_next_line/get_next_line.h
+++ lib/get_next_line/get_next_line.h
@@ -6,24 +6,26 @@
 /*   By: acastilh <acastilh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 22:06:30 by acastilh          #+#    #+#             */
-/*   Updated: 2024/04/03 20:00:28 by acastilh         ###   ########.fr       */
+/*   Updated: 2024/04/17 18:54:54 by acastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 # include <stdlib.h>
 # include <unistd.h>
 
-# define BUFFER_SIZE 32
-
 char	*get_next_line(int fd);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s);
-size_t	ft_strlen(const char *s);
-const char	*ft_strchr(const char *s, int c);
-char	*return_line(char *aux_str);
-char	*new_aux_str(char *aux_str);
+char	*read_line(int fd, char *str);
+char	*take_line(char *str);
+void	*gnl_calloc(int count, int size);
+char	*gnl_strchr(char *str, int c);
+char	*gnl_strjoin(char *s1, char *s2);
+size_t	gnl_strlen(char *str);
+char	*take_line(char *str);
+char	*clean_buffer(char *buffer);
 
 #endif
--- lib/get_next_line/get_next_line_utils.c
+++ lib/get_next_line/get_next_line_utils.c
@@ -6,120 +6,46 @@
 /*   By: acastilh <acastilh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 22:07:45 by acastilh          #+#    #+#             */
-/*   Updated: 2024/04/04 01:07:19 by acastilh         ###   ########.fr       */
+/*   Updated: 2024/04/17 18:53:53 by acastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
+void	*gnl_calloc(int count, int size)
 {
-	size_t	i;
+	char	*ptr;
+	size_t	len;
 
-	i = 0;
-	if (!s)
+	len = 0;
+	ptr = malloc (count * size);
+	if (!ptr)
 		return (0);
-	while (s[i])
-		i++;
-	return (i);
+	while (len < (unsigned int)(count * size))
+		*(ptr + len++) = 0;
+	return ((void *) ptr);
 }
 
-const char *ft_strchr(const char *s, int c)
+char	*gnl_strchr(char *str, int c)
 {
-    if (!s) return NULL;
-
-    while (*s != '\0') {
-        if (*s == (char)c) {
-            return (s);
-        }
-        s++;
-    }
-    if (c == '\0') {
-        return (s);
-    }
-    return (NULL);
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		if (*str == (unsigned char) c)
+			return (str);
+		str ++;
+	}
+	return (0);
 }
 
-
-char	*ft_strjoin(char const *s1, char const *s2)
+size_t	gnl_strlen(char *str)
 {
 	size_t	len;
-	char	*str;
-	char	*str_iter;
 
-	if (!s1 || !s2)
-	{
-		return (NULL);
-	}
-	len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	str = (char *)malloc(sizeof(char) * len);
-	if (!str)
-	{
-		return (NULL);
-	}
-	str_iter = str;
-	while (*s1 != '\0')
-	{
-		*str_iter++ = *s1++;
-	}
-	while (*s2 != '\0')
-	{
-		*str_iter++ = *s2++;
-	}
-	*str_iter = '\0';
-	return (str);
-}
-
-char	*return_line(char *aux_str)
-{
-	size_t	i;
-	char	*str;
-
-	i = 0;
-	if (!aux_str[i])
-		return (NULL);
-	while (aux_str[i] && aux_str[i] != '\n')
-		i++;
-	str = malloc(sizeof(char) * (i + 2));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (aux_str[i] && aux_str[i] != '\n')
-	{
-		str[i] = aux_str[i];
-		i++;
-	}
-	if (aux_str[i] == '\n')
-	{
-		str[i] = aux_str[i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
-
-char	*new_aux_str(char *aux_str)
-{
-	size_t	i;
-	size_t	a;
-	char	*str;
-
-	a = 0;
-	while (aux_str[a] && aux_str[a] != '\n')
-		a++;
-	if (!aux_str[a])
-	{
-		free(aux_str);
-		return (NULL);
-	}
-	str = malloc(sizeof(char) * (ft_strlen(aux_str) - a + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	a++;
-	while (aux_str[a])
-		str[i++] = aux_str[a++];
-	str[i] = '\0';
-	free(aux_str);
-	return (str);
+	len = 0;
+	if (str)
+		while (*(str + len))
+			len ++;
+	return (len);
 }
